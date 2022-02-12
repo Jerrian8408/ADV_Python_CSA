da6253c7de4166dfcc74ee56e653a1aa53b95ac7
Carlos Hernandez <HernandezCU@jbu.edu>
2022-02-12 10:33:46 -0600

    Initial commit

---
+++ CSV_Example.py
@@ -0,0 +1,51 @@
+import csv
+from pprint import pprint
+
+"""
+Opens the CSV File and stores all the data in an array
+Removes the header from the CSV File
+Converts String ID to Ints
+"""
+def import_data():
+    data = []
+    with open('JBUDB.csv') as db_data:
+        in_data = csv.reader(db_data, delimiter=',')
+        for r in in_data:
+            data.append(r)
+
+        del data[0]
+
+        for x in data:
+            x[0]=int(x[0])
+
+    return data
+
+
+def find_by_id(id: int):
+    return []
+
+
+def find_by_firstname(firstname: str):
+    return []
+
+
+def find_by_lastname(lastname: str):
+    return []
+
+
+def find_by_email(email: str):
+    return []
+
+
+def find_by_position(position: str):
+    return []
+
+
+def find_by_department(department: str):
+    return []
+
+
+if __name__ == '__main__':
+    pprint(import_data())
+
+    
